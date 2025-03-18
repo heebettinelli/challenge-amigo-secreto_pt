cd5df1811a021c9cfbbb837c8ce72d9d66b8e65f
Helen <fer.helen@icloud.com>
2025-03-18 15:08:41 -0300

    build JS, css changes and add html/css/js resources to clean up the form

--- app.js
+++ app.js
@@ -1,1 +1,92 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let copiaAmigos = [];
+
+window.onload = function() {
+    if (localStorage.getItem('amigos')) {
+        amigos = JSON.parse(localStorage.getItem('amigos'));
+        atualizarLista();
+    }
+}
+
+function adicionarAmigo(){
+    let inputAmigo = document.getElementById('amigo');
+    let nomeAmigo = inputAmigo.value;
+
+    if (nomeAmigo === ''){
+        alert('Digite o nome do amigo!');
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    atualizarLista();
+
+    localStorage.setItem('amigos', JSON.stringify(amigos));
+}
+
+function atualizarLista(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+            
+    for (let i = 0; i < amigos.length; i++){
+        let nome = document.createElement("li");
+        nome.textContent = amigos[i];
+        listaAmigos.appendChild(nome);
+    }
+}
+
+function limparLista(){
+    localStorage.removeItem('amigos');
+    amigos = [];
+    atualizarLista();
+}
+
+function sortearAmigo(nomeSorteador){
+    if (amigos.length === 0){
+        alert('Nenhum amigo adicionado!');
+        return;
+    }
+
+    if (copiaAmigos.length === 0) {
+        copiaAmigos = [...amigos];
+    }
+
+    let amigoSorteado;
+    let indiceSorteado;
+
+    do {
+        indiceSorteado = Math.floor(Math.random() * copiaAmigos.length);
+        amigoSorteado = copiaAmigos[indiceSorteado];
+    } while (amigoSorteado == nomeSorteador);
+
+    alert(`O amigo sorteado é: ${amigoSorteado}`);
+
+    copiaAmigos.splice(indiceSorteado, 1);
+
+
+    if (copiaAmigos.length === 0) {
+        alert('Todos os amigos já foram sorteados!');
+        limparLista();
+        resetarSorteio();
+    }
+}
+
+function resetarSorteio() {
+    copiaAmigos = [];
+    alert('A lista do sorteio foi resetada');
+}
+
+function limparCampos() {
+    document.querySelectorAll("input, textarea").forEach(field => field.value = "");
+
+    document.querySelectorAll("ul").forEach(list => list.innerHTML = "");
+
+    localStorage.removeItem("amigos");
+
+    amigos = [];
+    copiaAmigos = [];
+}
+
+
+
